47a5f8d2479d0d2ed9576a9e876b55c6a7c159dd
prateeksoni3103 <116192891+prateeksoni3103@users.noreply.github.com>
2022-10-20 23:10:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// The compile() Method in JavaScript is used to compile the regular expression while executing of script i.e compile the regular expression.
+// here compile function is used
+function compile() {
+    var html = document.getElementById("html");
+    var css = document.getElementById("css");
+    var js = document.getElementById("js");
+    var code = document.getElementById("code").contentWindow.document;
+  
+    document.body.onkeyup = function() {
+      code.open();
+      code.writeln(
+        html.value +
+          "<style>" +
+          css.value +
+          "</style>" +
+          "<script>" +
+          js.value +
+          "</script>"
+      );
+      code.close();
+    };
+  }
+  
+  compile();
+  
+
